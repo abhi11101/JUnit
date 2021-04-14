224996a2908bce8b4bec4f04cc297b34ca46dabe
Rock <thakurabhi1110@gmail.com>
2021-04-14 16:04:06 +0530

    Child branch activity

--- src/com/abhi/Main.java
+++ src/com/abhi/Main.java
@@ -3,6 +3,7 @@ package com.abhi;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main method...");
+        System.out.println("Child branch....");
     }
 
 }
