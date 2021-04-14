ab6e4aa76f2a15dbea3b9205726176d649feab87
Rock <thakurabhi1110@gmail.com>
2021-04-14 15:05:13 +0530

    Main added

--- src/com/abhi/BankAcoountTest.java
+++ src/com/abhi/BankAcoountTest.java
@@ -6,29 +6,36 @@ class BankAcoountTest {
 
     @org.junit.jupiter.api.Test
     void deposit() {
+        fail("This test has yet to be implemented!!");
     }
 
     @org.junit.jupiter.api.Test
     void withdraw() {
+        fail("This test has yet to be implemented!!");
     }
 
     @org.junit.jupiter.api.Test
     void getName() {
+        fail("This test has yet to be implemented!!");
     }
 
     @org.junit.jupiter.api.Test
     void setName() {
+        fail("This test has yet to be implemented!!");
     }
 
     @org.junit.jupiter.api.Test
     void getBalance() {
+        fail("This test has yet to be implemented!!");
     }
 
     @org.junit.jupiter.api.Test
     void setBalance() {
+        fail("This test has yet to be implemented!!");
     }
 
     @org.junit.jupiter.api.Test
     void testToString() {
+        fail("This test has yet to be implemented!!");
     }
 }
---
+++ src/com/abhi/Main.java
@@ -0,0 +1,8 @@
+package com.abhi;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main method...");
+    }
+
+}
